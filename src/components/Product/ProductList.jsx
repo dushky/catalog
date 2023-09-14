f5dd34e3df90215a42aa3e5215be5b865272dc6d
dushky <dusanbako100@gmail.com>
2023-09-15 01:14:27 +0200

    implement infinite scroll and expand database

--- src/components/Product/ProductList.jsx
+++ src/components/Product/ProductList.jsx
@@ -1,26 +1,34 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, useRef } from "react";
 import axios from "axios";
 import ProductBox from "./ProductBox";
 import "./ProductList.css";
 import FilterContext from "../../store/filter-context";
 
 const ProductList = () => {
-  const [products, setProducts] = useState();
+  const [products, setProducts] = useState([]);
+  const [productCount, setProductCount] = useState(0);
   const ctx = useContext(FilterContext);
+  const totalProducts = useRef(0);
+
   useEffect(() => {
     const source = axios.CancelToken.source();
     let categoryQuery = "";
-    ctx.filteredCategories.map((fc) => {
-      categoryQuery = `${categoryQuery}&category_id=${fc}`;
+    ctx.filteredCategories.map((categoryId) => {
+      categoryQuery = `${categoryQuery}&category_id=${categoryId}`;
     });
-    const url = `http://localhost:8000/products/?_start=0&_end=20${categoryQuery}`;
+
+    const url = `http://localhost:8000/products/?_start=0&_end=${
+      productCount + 20
+    }${categoryQuery}`;
 
     axios
       .get(url, {
         cancelToken: source.token,
       })
       .then((response) => {
-        setProducts(response.data);
+        const data = response.data;
+        totalProducts.current = response.headers["x-total-count"];
+        setProducts(data);
       })
       .catch((error) => {
         if (axios.isCancel(error)) {
@@ -29,10 +37,30 @@ const ProductList = () => {
           console.error(error);
         }
       });
+
     return () => {
       source.cancel();
     };
-  }, [ctx.filteredCategories]);
+  }, [ctx.filteredCategories, productCount]);
+
+  useEffect(() => {
+    function handleScroll() {
+      if (
+        window.innerHeight + window.scrollY >=
+          document.body.offsetHeight - 200 &&
+        productCount < totalProducts.current - 20 //prevent loading of non existing records
+      ) {
+        //when user is near bottom of the page
+        setProductCount(productCount + 20);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [products]);
 
   return (
     <div className="product-list">
@@ -42,6 +70,8 @@ const ProductList = () => {
         })}
 
       {/*Thisfixes layout of last row of products*/}
+      <div className="product-box" style={{ visibility: "hidden" }}></div>
+      <div className="product-box" style={{ visibility: "hidden" }}></div>
       <div className="product-box" style={{ visibility: "hidden" }}></div>
       <div className="product-box" style={{ visibility: "hidden" }}></div>
       <div className="product-box" style={{ visibility: "hidden" }}></div>
